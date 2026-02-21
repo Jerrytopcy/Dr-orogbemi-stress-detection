bccdd5c8a11cce727154ca8a06bc756159de6032
JerrytopcyCode <115675790+Jerrytopcy@users.noreply.github.com>
2026-02-21 19:03:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Default route to index.html
 app.get('/', (req, res) => {
-  res.send('Stress Detection Server is Running 🚀');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
